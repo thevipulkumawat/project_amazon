9d9ecea8b0d8dd0874badb30ec9d8fcafe541aef
thevipulkumawat <vipullkumawat2000@gmail.com>
2022-11-09 15:41:43 +0530

    09112022

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,11 +3,14 @@ const User = require('../models/user');
 
 const authRouter = express.Router();
 
-authRouter.post('/api/signup', (req, res) => {
+authRouter.post('/api/signup', async (req, res) => {
    // getting data from client side
     const {name, email, password} = req.body;
 
-    const existingUser = User.findOne({email });
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.json
+    }
    // post that data in database
 
 
